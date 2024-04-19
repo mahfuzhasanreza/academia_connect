0dbf549b85e92e7a9ab022346fb638d0c0fdae19
mahfuzhr <mahfuzhasanreza1@gmail.com>
2024-04-19 21:14:45 +0600

    add updated modify files | mahfuz

--- sql/academia_connect.sql
+++ sql/academia_connect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2024 at 07:26 AM
+-- Generation Time: Apr 19, 2024 at 05:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -187,32 +187,13 @@ CREATE TABLE `teacher_availability` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(100) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `role` enum('student','teacher','club_manager','canteen_staff','gymnasium_manager','shuttle_driver') NOT NULL,
-  `fullname` varchar(50) DEFAULT NULL,
-  `full_name` varchar(50) DEFAULT NULL
+  `role` varchar(100) NOT NULL,
+  `timestamp` int(11) NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `fullname`, `full_name`) VALUES
-(1, 'Emon', '111', '', 'student', NULL, NULL),
-(2, 'abc', '123', '', 'student', NULL, NULL),
-(3, 'pqr', '456', '', 'student', NULL, NULL),
-(4, 'Emon143Nerd', '$2y$10$plfG9EnXFvwkJmKlBjvrVe6sCJwK/iSik1xC80djWqgbYxveLmMqu', 'emon99284@gmail.com', 'student', NULL, 'Md Emon'),
-(5, 'busDriver1212', '$2y$10$s/o7ASth3/kHjdkcZcs/Te8ofe9Hi1JzJP8KlKEqIoAs1Svmi5ERm', 'driver@gmail.com', 'shuttle_driver', NULL, 'Bus Driver'),
-(6, 'bashir1212', '$2y$10$rczOmg1LQkcX3NQH3m53Ue.ecL3Z0ouJRP3DybTBrdqYbQQCrMehS', 'bashir@gmail.com', 'teacher', NULL, 'Mahtab Bashir'),
-(7, 'canteenStaff', '$2y$10$O47SzSWEcjUfoE2XaI6xPu9Nnh7N9WoCG3hEGu5FIWU.6LRAC3dwq', 'staff@gmail.com', 'canteen_staff', NULL, 'canteen Staff'),
-(8, 'clubManager', '$2y$10$tNSUhaJzgy60u25w42z/WeeBq25pzYySe5QG8yrGSR1TQEnRKvamG', 'club@gmail.com', 'club_manager', NULL, 'club manager'),
-(9, 'gymManager', '$2y$10$gRtA5VfUYJwSOUxV4m0Zpu37UAxtYXKGPNK83lVyt5m9KE6qRpWSC', 'gym@gmail.com', '', NULL, 'gym manager'),
-(10, 'gym123', '$2y$10$C2/aEhc5Lak3NihBd40R3OaLkhsTYtWntJZSWqQYBcig/pbXaW5Wm', 'gymnasium@gmail.com', '', NULL, 'gym gym'),
-(11, 'asif123', '$2y$10$3HCRY1smBZ84dSrrlVC6g.VTyLSbj127mQRG9odlHyiM4Q9.CtBMO', 'asif@gmail.com', 'teacher', NULL, 'asif ahmed'),
-(12, 'emon123', '$2y$10$2LAw/nHWB0UAGWIjQJGby.iyEF6X0Z2SYvPyNcwxbtY35En6KWk5W', 'emon123@gmail.com', 'student', NULL, 'emon bepari');
 
 --
 -- Indexes for dumped tables
@@ -306,7 +287,8 @@ ALTER TABLE `teacher_availability`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -388,7 +370,7 @@ ALTER TABLE `teacher_availability`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -398,63 +380,63 @@ ALTER TABLE `users`
 -- Constraints for table `canteen_staff`
 --
 ALTER TABLE `canteen_staff`
-  ADD CONSTRAINT `canteen_staff_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `canteen_staff_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `clubs`
 --
 ALTER TABLE `clubs`
-  ADD CONSTRAINT `clubs_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `clubs_ibfk_1` FOREIGN KEY (`manager_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `club_members`
 --
 ALTER TABLE `club_members`
-  ADD CONSTRAINT `club_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `club_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `club_members_ibfk_2` FOREIGN KEY (`club_id`) REFERENCES `clubs` (`id`);
 
 --
 -- Constraints for table `counseling_availability`
 --
 ALTER TABLE `counseling_availability`
-  ADD CONSTRAINT `counseling_availability_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `counseling_availability_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `counselling_requests`
 --
 ALTER TABLE `counselling_requests`
-  ADD CONSTRAINT `counselling_requests_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `counselling_requests_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `gymnasium_manager`
 --
 ALTER TABLE `gymnasium_manager`
-  ADD CONSTRAINT `gymnasium_manager_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `gymnasium_manager_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `gymnasium_membership`
 --
 ALTER TABLE `gymnasium_membership`
-  ADD CONSTRAINT `gymnasium_membership_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `gymnasium_membership_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `marketplace_items`
 --
 ALTER TABLE `marketplace_items`
-  ADD CONSTRAINT `marketplace_items_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `marketplace_items_ibfk_2` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `marketplace_items_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `marketplace_items_ibfk_2` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `shuttle_driver`
 --
 ALTER TABLE `shuttle_driver`
-  ADD CONSTRAINT `shuttle_driver_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `shuttle_driver_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `teacher_availability`
 --
 ALTER TABLE `teacher_availability`
-  ADD CONSTRAINT `teacher_availability_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `teacher_availability_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
